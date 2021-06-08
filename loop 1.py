1fa4a8fb3a9b2fb8bd5d03cc08b4e3588d6e4e7b
tarunsaroha <tarun20@navgurukul.org>
2021-06-08 14:28:10 +0530

    push if else code

---
+++ loop 1.py
@@ -0,0 +1,15 @@
+row=int(input("enter the row"))
+colum=int(input("enter the"))
+while i<=1:
+        if i%2!=0:
+        print("1",end=" ")
+        j=j+1
+    k=i
+    while k<=i:
+        print("0",end=" ")
+        k=k+1
+    print()
+    i=i+1
+
+         
+
